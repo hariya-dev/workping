9c70a87523371dcb061d82909d16cd0ed4c4a7df
Trần Đức Hải <hai2000.dev@gmail.com>
2026-04-27 19:01:58 +0700

    init

---
+++ Frontend/employee-management/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// environments/environment.prod.ts
+// Cau hinh moi truong Production
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.yourcompany.com',
+  appName: 'He thong Quan ly Nhan su',
+  companyName: 'An Tuong Technology'
+};
